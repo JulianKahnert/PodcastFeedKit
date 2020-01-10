6855d343b1734ba5f52e20646f1c5e1e150ec1f5
Callum Kerr-Edwards <github@callumkerredwards.co.uk>
2020-01-10 17:25:38 +0000

    Adding overall feed test

--- Tests/PodcastFeedKitTests/PodcastFeedKitTests.swift
+++ Tests/PodcastFeedKitTests/PodcastFeedKitTests.swift
@@ -2,39 +2,8 @@ import XCTest
 @testable import PodcastFeedKit
 
 final class PodcastFeedKitTests: XCTestCase {
-    func testFeedGeneratesWithMinimalAttributes() {
 
-        let expectedOutput = """
-        <?xml version="1.0" encoding="utf-8" standalone="no"?>
-        <rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
-            <channel>
-                <title>Test Podcast Title</title>
-                <link>https://demo.url/feed.rss</link>
-            </channel>
-        </rss>
-        """
-        XCTAssertEqual(Podcast(title: "Test Podcast Title", link: "https://demo.url/feed.rss").getFeed(), expectedOutput)
-    }
-
-    func testFeedGeneratesWithCleanAttribute() {
-
-        let expectedOutput = """
-        <?xml version="1.0" encoding="utf-8" standalone="no"?>
-        <rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
-            <channel>
-                <title>Test Podcast Title</title>
-                <link>https://demo.url/feed.rss</link>
-                <itunes:explicit>no</itunes:explicit>
-            </channel>
-        </rss>
-        """
-        XCTAssertEqual(Podcast(title: "Test Podcast Title",
-                               link: "https://demo.url/feed.rss")
-                               .containsExplicitMaterial(false)
-                               .getFeed(), expectedOutput)
-    }
-
-    func testFeedGeneratesWithExplicitAttribute() {
+    func testFullFeedGeneration() {
 
         let expectedOutput = """
         <?xml version="1.0" encoding="utf-8" standalone="no"?>
@@ -43,56 +12,19 @@ final class PodcastFeedKitTests: XCTestCase {
                 <title>Test Podcast Title</title>
                 <link>https://demo.url/feed.rss</link>
                 <itunes:explicit>yes</itunes:explicit>
+                <language>en</language>
             </channel>
         </rss>
         """
         XCTAssertEqual(Podcast(title: "Test Podcast Title",
                                link: "https://demo.url/feed.rss")
                                .containsExplicitMaterial()
+                               .withLanguageCode(Language.english.rawValue)
                                .getFeed(), expectedOutput)
     }
 
-   func testFeedGeneratesWithBlockAttribute() {
-
-       let expectedOutput = """
-       <?xml version="1.0" encoding="utf-8" standalone="no"?>
-       <rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
-           <channel>
-               <title>Test Podcast Title</title>
-               <link>https://demo.url/feed.rss</link>
-               <itunes:block>Yes</itunes:block>
-           </channel>
-       </rss>
-       """
-       XCTAssertEqual(Podcast(title: "Test Podcast Title",
-                              link: "https://demo.url/feed.rss")
-                              .blockFromITunes()
-                              .getFeed(), expectedOutput)
-   }
-
-   func testFeedGeneratesWithLanguageCode() {
-
-       let expectedOutput = """
-       <?xml version="1.0" encoding="utf-8" standalone="no"?>
-       <rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
-           <channel>
-               <title>Test Podcast Title</title>
-               <link>https://demo.url/feed.rss</link>
-               <language>en-gb</language>
-           </channel>
-       </rss>
-       """
-       XCTAssertEqual(Podcast(title: "Test Podcast Title",
-                              link: "https://demo.url/feed.rss")
-                              .withLanguageCode(Language.english_uk.rawValue)
-                              .getFeed(), expectedOutput)
-   }
-
     static var allTests = [
-        ("testFeedGeneratesWithMinimalAttributes", testFeedGeneratesWithMinimalAttributes),
-        ("testFeedGeneratesWithCleanAttribute", testFeedGeneratesWithCleanAttribute),
-        ("testFeedGeneratesWithExplicitAttribute", testFeedGeneratesWithExplicitAttribute),
-        ("testFeedGeneratesWithBlockAttribute", testFeedGeneratesWithBlockAttribute),
-        ("testFeedGeneratesWithLanguageCode", testFeedGeneratesWithLanguageCode)
+        ("testFullFeedGeneration", testFullFeedGeneration)
     ]
+
 }
---
+++ Tests/PodcastFeedKitTests/PodcastTests.swift
@@ -0,0 +1,98 @@
+import XCTest
+@testable import PodcastFeedKit
+
+final class PodcastTests: XCTestCase {
+    func testFeedGeneratesWithMinimalAttributes() {
+
+        let expectedOutput = """
+        <?xml version="1.0" encoding="utf-8" standalone="no"?>
+        <rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
+            <channel>
+                <title>Test Podcast Title</title>
+                <link>https://demo.url/feed.rss</link>
+            </channel>
+        </rss>
+        """
+        XCTAssertEqual(Podcast(title: "Test Podcast Title", link: "https://demo.url/feed.rss").getFeed(), expectedOutput)
+    }
+
+    func testFeedGeneratesWithCleanAttribute() {
+
+        let expectedOutput = """
+        <?xml version="1.0" encoding="utf-8" standalone="no"?>
+        <rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
+            <channel>
+                <title>Test Podcast Title</title>
+                <link>https://demo.url/feed.rss</link>
+                <itunes:explicit>no</itunes:explicit>
+            </channel>
+        </rss>
+        """
+        XCTAssertEqual(Podcast(title: "Test Podcast Title",
+                               link: "https://demo.url/feed.rss")
+                               .containsExplicitMaterial(false)
+                               .getFeed(), expectedOutput)
+    }
+
+    func testFeedGeneratesWithExplicitAttribute() {
+
+        let expectedOutput = """
+        <?xml version="1.0" encoding="utf-8" standalone="no"?>
+        <rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
+            <channel>
+                <title>Test Podcast Title</title>
+                <link>https://demo.url/feed.rss</link>
+                <itunes:explicit>yes</itunes:explicit>
+            </channel>
+        </rss>
+        """
+        XCTAssertEqual(Podcast(title: "Test Podcast Title",
+                               link: "https://demo.url/feed.rss")
+                               .containsExplicitMaterial()
+                               .getFeed(), expectedOutput)
+    }
+
+   func testFeedGeneratesWithBlockAttribute() {
+
+       let expectedOutput = """
+       <?xml version="1.0" encoding="utf-8" standalone="no"?>
+       <rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
+           <channel>
+               <title>Test Podcast Title</title>
+               <link>https://demo.url/feed.rss</link>
+               <itunes:block>Yes</itunes:block>
+           </channel>
+       </rss>
+       """
+       XCTAssertEqual(Podcast(title: "Test Podcast Title",
+                              link: "https://demo.url/feed.rss")
+                              .blockFromITunes()
+                              .getFeed(), expectedOutput)
+   }
+
+   func testFeedGeneratesWithLanguageCode() {
+
+       let expectedOutput = """
+       <?xml version="1.0" encoding="utf-8" standalone="no"?>
+       <rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
+           <channel>
+               <title>Test Podcast Title</title>
+               <link>https://demo.url/feed.rss</link>
+               <language>en-gb</language>
+           </channel>
+       </rss>
+       """
+       XCTAssertEqual(Podcast(title: "Test Podcast Title",
+                              link: "https://demo.url/feed.rss")
+                              .withLanguageCode(Language.english_uk.rawValue)
+                              .getFeed(), expectedOutput)
+   }
+
+    static var allTests = [
+        ("testFeedGeneratesWithMinimalAttributes", testFeedGeneratesWithMinimalAttributes),
+        ("testFeedGeneratesWithCleanAttribute", testFeedGeneratesWithCleanAttribute),
+        ("testFeedGeneratesWithExplicitAttribute", testFeedGeneratesWithExplicitAttribute),
+        ("testFeedGeneratesWithBlockAttribute", testFeedGeneratesWithBlockAttribute),
+        ("testFeedGeneratesWithLanguageCode", testFeedGeneratesWithLanguageCode)
+    ]
+}
